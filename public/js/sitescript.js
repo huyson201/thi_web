bf7cf27561866722d1315c6deb5d2dde84773c2e
HuySon <huyson201@gmail.com>
2021-04-08 21:40:29 +0700

    update

--- public/js/sitescript.js
+++ public/js/sitescript.js
@@ -1,16 +1,36 @@
 // stickbar active
 const stickBar = document.querySelector('#stick-bar');
+const toTopBtn = document.getElementById('to-top-btn');
+var isScrolling;
 
 window.onscroll = function(){
+
     if(window.pageYOffset > 100){
         stickBar.classList.add('active');
+        toTopBtn.classList.add('active');
     }
 
     if(window.pageYOffset < 100){
         if(stickBar.classList.contains('active')) stickBar.classList.remove('active');
+        if(toTopBtn.classList.contains('active')) toTopBtn.classList.remove('active');
     }
+    // Clear our timeout throughout the scroll
+	window.clearTimeout( isScrolling );
+
+	// Set a timeout to run after scrolling ends
+	isScrolling = setTimeout(function() {
+        toTopBtn.classList.remove('active');
+	}, 3000);
+ 
 }
 
+//  to top btn event
+ function toTop(){
+    window.scrollTo({      
+        top:0,  
+        behavior:"smooth",    
+    })
+}
 // left-nav event
 
 const leftNav = document.querySelector('.left-navigation');
@@ -25,3 +45,5 @@ hamburgerBtns.forEach( (e) =>{
 clsBtn.onclick = function(){
     leftNav.classList.remove('active')
 }
+
+
